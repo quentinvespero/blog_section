36d834aee7cbe15b31954525692fa214c56aa0ae
quentinvespero <quentinvespero@gmail.com>
2023-05-09 02:10:17 +0200

    starting architecture app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './style/index.scss'
+import Articles from './components/Article'
+import Sidebar from './components/Sidebar'
 
 function App() {
     return (
-        <div className="App"></div>
+        <div className="appComponent">
+            <Sidebar/>
+            <div className="divider"></div>
+            <Articles/>
+        </div>
     )
 }
 
---
+++ src/components/Article.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Article = () => {
+    return (
+        <div className='articleComponent'>
+            <h1>titreTest</h1>
+
+        </div>
+    )
+}
+
+export default Article
